b5c0bad69fec496bdfa48f800c0d2a8e91cb6092
Yusen22 <elijahwilsher@gmail.com>
2023-01-16 19:10:28 +0000

    added questions totalling 20 different questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ var questionNumber = quizQuestions
 var quizQuestions = [
 
     {
-        title: "What does JSON stand for?",
+        title: "1. What does JSON stand for?",
         answer1: "1. Janky Stupid Ode to Newts",
         answer2: "2. JavaScript Orientation Node",
         answer3: "3. JavaScience Oscillating Neurons",
@@ -22,7 +22,7 @@ var quizQuestions = [
         correctAnswerIndex: 4
     },
     {
-        title: "Which HTML element links JavaScript to the DOM?",
+        title: "2. Which HTML element links JavaScript to the DOM?",
         answer1: "1. <link> ",
         answer2: "2. <script> ",
         answer3: "3. <source>",
@@ -30,7 +30,7 @@ var quizQuestions = [
         correctAnswerIndex: 2
     },
     {
-        title: "Which of these acts as an ID selector in JavaSript?",
+        title: "3. Which of these acts as an ID selector in JavaSript?",
         answer1: "1. #selectedID",
         answer2: "2. id.selectedID",
         answer3: "3. .selectedID",
@@ -38,7 +38,7 @@ var quizQuestions = [
         correctAnswerIndex: 1,
     },
     {
-        title: "What does HTML stand for?",
+        title: "4. What does HTML stand for?",
         answer1: "1. HighText Mark Language",
         answer2: "2. Heavy Light Truly Lasting",
         answer3: "3. HyperText Markup Language",
@@ -46,7 +46,7 @@ var quizQuestions = [
         correctAnswerIndex: 3
     },
     {
-        title: "Which Git command adds a message to the commit?",
+        title: "5. Which Git command adds a message to the commit?",
         answer1: "1. git commit -msg 'message here'",
         answer2: "2. git commit -m 'message here'",
         answer3: "3. git message 'message here'",
@@ -54,13 +54,125 @@ var quizQuestions = [
         correctAnswerIndex: 2
     },
     {
-        title: "Which example shows the correct way to write a function",
+        title: "6. Which example shows the correct way to write a function?",
         answer1: "var newFunction = function() {}",
         answer2: "var newFunction() {}",
         answer3: "function(), newFunction {}",
         answer4: "var newFunction()function {}",
         correctAnswerIndex: 1
-    }
+    },
+    {
+        title: "7. What does DOM stand for?",
+        answer1: "District On Mutation",
+        answer2: "Dimunitive Open Moel",
+        answer3: "Deregistered On Setting",
+        answer4: "Document Object Model",
+        correctAnswerIndex: 4
+    },
+    {
+        title: "8. Which is NOT a JavaScript data type?",
+        answer1: "Boolean",
+        answer2: "String",
+        answer3: "CSS",
+        answer4: "Number",
+        correctAnswerIndex: 3
+    },
+    {
+        title: "9. In CSS, what does the 'padding' property do?",
+        answer1: "Compresses the content inside the container",
+        answer2: "Creates space around the content within the container",
+        answer3: "Creates space outside of the container",
+        answer4: "Adds another layer to the selected element",
+        correctAnswerIndex: 2
+    },
+    {
+        title: "10. Which of these HTML elements give the largest heading?",
+        answer1: "<head>",
+        answer2: "<h1>",
+        answer3: "<h4>",
+        answer4: "<header>",
+        correctAnswerIndex: 2
+    },
+    {
+        title: "11. Which HTML element links CSS to the DOM?",
+        answer1: "<link>",
+        answer2: "<css>",
+        answer3: "<script>",
+        answer4: "<pattern>",
+        correctAnswerIndex: 1
+    },
+    {
+        title: "12. What is the correct HTML for creating a hyperlink?",
+        answer1: "<a url='http://www.examplesite.com'>Example Site</a>",
+        answer2: "<a name='http://www.examplesite.com'>Example Site</a>",
+        answer3: "<hl url='http://www.examplesite.com'>Example Site</hl>",
+        answer4: "<a href='http://www.examplesite.com'>Example Site</a>",
+        correctAnswerIndex: 4
+    },
+    {
+        title: "13. Which application is used to store repositories and multiple commits?",
+        answer1: "GitLab",
+        answer2: "GitHub",
+        answer3: "Google Repos",
+        answer4: "RepoIndex",
+        correctAnswerIndex: 1
+    },
+    {
+        title: "14. Which is the correct example of JavaScipt Object",
+        answer1: "{property: 'me'}",
+        answer2: "[property: 'me']",
+        answer3: "<property: 'me'>",
+        answer4: "(property: 'me')",
+        correctAnswerIndex: 1
+    },
+    {
+        title: "15. A 'confirm' fuction in JavaScript...",
+        answer1: "Alert users of provided info",
+        answer2: "Ask user for a confirm/deny answer",
+        answer3: "Allow user to enter a value",
+        answer4: "Turn the screen red",
+        correctAnswerIndex: 2
+    },
+    {
+        title: "16. Javascript 'strings' must be contained in...",
+        answer1: "Curly brackets",
+        answer2: "Parentheses",
+        answer3: "Quotation marks",
+        answer4: "Repository",
+        correctAnswerIndex: 3
+    },
+    {
+        title: "17. Choose the option which is NOT a framework for CSS or JavaScript",
+        answer1: "Angular",
+        answer2: "Mirage",
+        answer3: "React",
+        answer4: "Bootstrap",
+        correctAnswerIndex: 2
+    },
+    {
+        title: "A professional repository should contain...",
+        answer1: "A selfie",
+        answer2: "Pictures of the console readings",
+        answer3: "A personal message to users",
+        answer4: "A quality README",
+        correctAnswerIndex: 4
+    },
+    {
+        title: "19. What is the design template that is used by a developer called?",
+        answer1: "DesignBoard",
+        answer2: "Wireframe",
+        answer3: "Markup",
+        answer4: "Mood board",
+        correctAnswerIndex: 2
+    },
+    {
+        title: "20. How often should you 'git push' your code to a repository?",
+        answer1: "At the start of the project",
+        answer2: "At the end of the project",
+        answer3: "At every change you want to save and maintain",
+        answer4: "When the code shows an error message in the console",
+        correctAnswerIndex: 3
+    },
 
 ]
 
