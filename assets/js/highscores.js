976a6f41181b1e2dfcf6f9a2eb5340c4979986f3
Yusen22 <elijahwilsher@gmail.com>
2023-01-16 01:10:55 +0000

    sorted printing highscores, showing top ten

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -3,22 +3,29 @@ var highScoresDiv = document.querySelector('#highscores')
 var highScores = JSON.parse(localStorage.getItem("highscores"))
 console.log(highScores)
 
+var sortedHighScores = highScores.sort(function(a, b) {
+    return parseFloat(a.score) - parseFloat(b.score);
+}).reverse();
+
+
+
+
 
 
 
 
 var printHighScore = () => {
 
-    if (highScores < 11) {
-        for (var x = 0; x < highScores.length; x++) {
+    
+        for (var x = 0; x < 10; x++) {
             var li = document.createElement("li");
-            var forHighScore = highScores[x];
+            var forHighScore = sortedHighScores[x];
             var initialAndScore = "Initials: " + forHighScore.initials + "   " + "Score: " + forHighScore.score;
             li.setAttribute("data-index", x);
             li.textContent = initialAndScore;
             highScoresDiv.appendChild(li)
         }
-    }
+    
 }
 
 printHighScore();
